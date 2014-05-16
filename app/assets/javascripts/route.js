898cabba98f3fc304ae7da039d3634b3597ca159
Rajarshi Das <rajarshid@cybage.com>
2014-05-16 19:00:37 +0530

    Port from jquery to angular js

--- app/assets/javascripts/route.js
+++ app/assets/javascripts/route.js
@@ -1,10 +1,14 @@
 var module = angular.module('productapp', ['ngSanitize']);
-module.controller('ProductsController', function ($scope) {
 
-  $scope.products = [{
-          title: '<p><em>Pizza1 </em> It is a maxican Pizza</p>',
-          images: '/images/pizza1.jpg',
-          price: 82
-  }];
+module.controller('OrdersController',  function ($scope, $http) {
+
+     $scope.order = function(){
+        $http({method: 'GET', url: '/orders/new'}).success(function(data, status, headers, config){
+        console.log(data);
+        $scope.data = data;
+        }
+    );
+
+  }
 
 });
